1eb6e8b7dda1f683173505c9a04c1f5c921560b4
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-01-03 12:59:49 +0000

    JS in progress

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ toggle.addEventListener('click', function() {
     toggle.classList.toggle('active')
     options.classList.toggle('active')
 })
+
+//To check if passwords match
+var password= document.document.getElementById('password');
+var confirmation= document.getElementById('confirm_password');
+
+if (confirmation.value != password) {
+    alert("Passwords do not match")
+}
