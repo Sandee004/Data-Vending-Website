0c038f49da66c02b3cc9215be0f76a77f36cdc0d
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-01-03 21:37:33 +0000

    Rearranged files

--- script.js
+++ script.js
@@ -21,10 +21,3 @@ toggle.addEventListener('click', function() {
 })
 
 //To check if passwords match
-var password= document.getElementById('password');
-var confirmation= document.getElementById('confirm_password');
-var reg_btn= document.getElementById('submit');
-
-reg_btn.addEventListener("onclick", function() {
-        alert("Passwords do not match")
-})
