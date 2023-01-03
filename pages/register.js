de01c562c634e9a5387baae7750a9f9f0b8b04fb
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-01-03 23:25:43 +0000

    Full name error message done

--- pages/register.js
+++ pages/register.js
@@ -1,8 +1,16 @@
+var fullname= document.getElementById('fullname');
+var fullname_error= document.getElementById('fullname_error');
+
 var password= document.getElementById('password');
 var confirmation= document.getElementById('confirm_password');
 var submit= document.getElementById('submit');
 
-submit.addEventListener("click", function() {
-    //ev.preventDefault()
-    alert("Passwords do not match")
+submit.addEventListener("click", function(ev) {
+    if (fullname.value == "") {
+        ev.preventDefault()
+        fullname_error.innerHTML= "Cannot be blank"
+        if (fullname.hasFocus()===True){
+            alert("Progress")
+        }
+    }
 })
