bbb465ab8e17225c63370fb549b5c78fa5661495
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-01-03 22:55:01 +0000

    Error messages in progress

---
+++ pages/register.js
@@ -0,0 +1,8 @@
+var password= document.getElementById('password');
+var confirmation= document.getElementById('confirm_password');
+var submit= document.getElementById('submit');
+
+submit.addEventListener("click", function() {
+    //ev.preventDefault()
+    alert("Passwords do not match")
+})
