4aef407802bad509f463659a39a19430386e6ac1
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-01-04 00:06:36 +0000

    Full name Validation completed

--- pages/register.js
+++ pages/register.js
@@ -6,11 +6,17 @@ var confirmation= document.getElementById('confirm_password');
 var submit= document.getElementById('submit');
 
 submit.addEventListener("click", function(ev) {
+    //Fullname Validation
     if (fullname.value == "") {
         ev.preventDefault()
+        fullname.style.borderColor="red"
         fullname_error.innerHTML= "Cannot be blank"
-        if (fullname.hasFocus()===True){
-            alert("Progress")
-        }
+        if (fullname.addEventListener("focus", function() {
+            fullname_error.innerHTML= ""
+            fullname.style.borderColor="grey"
+        }))
+    alert("Useless line of code")
     }
+
+    //Username
 })
