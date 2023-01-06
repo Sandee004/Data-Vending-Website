5083db8f93bb1c568214f2c11ea073de91e42991
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-01-06 20:04:38 +0000

    Dashboard next

--- pages/login.js
+++ pages/login.js
@@ -1,22 +1,15 @@
 var User= document.getElementById('Username');
-var User_error= document.getElementById('User_error');
-var Password= document.getElementById('Password');
-var Password_error= document.getElementById('Password_error');
+var Password= document.getElementById('Password')
 var login= document.getElementById('button');
 var nameInStorage= localStorage.getItem("user-name")
 var passInStorage= localStorage.getItem("user-pass")
-//ar grp= document.getElementByClassName("error-messages")
 
 login.addEventListener("click", function(ev) {
-    if (User.value == "") {
+     if (User.value == "") {
         ev.preventDefault()
         User.style.borderColor="red"
-        User_error.style.color= "red"
-        User_error.innerHTML= "Cannot be blank"
         if (User.addEventListener("focus", function() {
             User.style.borderColor="grey"
-            User_error.innerHTML= ""
-            User_error.style.color= "grey"
         }))
      alert("Useless line of code")
     }
@@ -24,22 +17,18 @@ login.addEventListener("click", function(ev) {
      if (Password.value == "") {
         ev.preventDefault()
         Password.style.borderColor="red"
-        Password_error.style.color= "red"
-        Password_error.innerHTML= "Passwords do not match"
         if (Password.addEventListener("focus", function() {
             Password.style.borderColor="grey"
-            Password_error.innerHTML= ""
-            Password_error.style.color= "grey"
         }))
      alert("Useless line of code")
-     }
+}
     
     if (User.value == nameInStorage && Password.value == passInStorage){
     ev.preventDefault()
     window.open("homepage.html", "_self");
     }
         
-    else{
+    if (User.value !== nameInStorage && User.value.length > 0 || Password.value !== passInStorage && Password.value.length > 0){
         ev.preventDefault()
         alert('Invalid Credentials')
     }
