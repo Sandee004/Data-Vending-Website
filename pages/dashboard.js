0249e16db376cce4d57bf2df5d4b1586faa68afc
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-01-12 14:00:50 +0000

    Fixed the navbar on the dashboard and added the sidebar

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,3 +1,27 @@
+//To get a value from local storage 
 const introduction= document.getElementById('introduction');
 let username= localStorage.getItem("user-name")
 introduction.innerHTML="Good day, "+ username
+
+//For the navbar
+const toggle = document.getElementById('toggle')
+const options = document.getElementById('option')
+
+document.body.onscroll = function(e) {
+    if (e.target.id !== 'options' && e.target.id !== 'toggle') {
+        toggle.classList.remove('active')
+        options.classList.remove('active')
+    }
+}
+
+document.body.onclick = function(e) {
+    if (e.target.id !== 'options' && e.target.id !== 'toggle') {
+        toggle.classList.remove('active')
+        options.classList.remove('active')
+    }
+}
+
+toggle.addEventListener('click', function() {
+    toggle.classList.toggle('active')
+    options.classList.toggle('active')
+})
