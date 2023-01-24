d2520cd947611c531c2f654763f76b8ffb76023d
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-01-24 21:48:45 +0000

    Account summary do over

---
+++ pages/acc-summary.js
@@ -0,0 +1,22 @@
+//For the navbar
+const toggle = document.getElementById('toggle')
+const options = document.getElementById('option')
+
+document.body.onscroll = function(e) {
+    if (e.target.id !== 'options' && e.target.id !== 'toggle') {
+        toggle.classList.remove('active')
+        options.classList.remove('active')
+    }
+}
+
+document.body.onclick = function(e) {
+    if (e.target.id !== 'options' && e.target.id !== 'toggle') {
+        toggle.classList.remove('active')
+        options.classList.remove('active')
+    }
+}
+
+toggle.addEventListener('click', function() {
+    toggle.classList.toggle('active')
+    options.classList.toggle('active')
+})
