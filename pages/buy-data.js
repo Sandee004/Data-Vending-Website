d5130845cae175adb09a5ecb06507d03da2e62da
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-02-18 23:28:35 +0000

    Done with main functionality on the buy data page. Remaining to add pin authorization

--- pages/buy-data.js
+++ pages/buy-data.js
@@ -19,3 +19,73 @@ toggle.addEventListener('click', function() {
     toggle.classList.toggle('active')
     options.classList.toggle('active')
 })
+
+
+var btn = document.getElementById('btn');
+var select1 = document.getElementById("select1")
+var network = document.getElementById('network');
+var dataType = document.getElementById('dataType');
+var gloDiv = document.getElementById('gloDiv');
+var etisalatDiv = document.getElementById('etisalatDiv');
+var airtelDiv = document.getElementById('airtelDiv');
+
+var mtnCG1 = document.getElementById('cg1Div');
+var CG1options = document.getElementById('CG1options');
+var CG2options = document.getElementById('CG2options');
+var SMEoptions = document.getElementById('SMEoptions');
+var mtnCG2 = document.getElementById('cg2Div');
+var mtnSME = document.getElementById('smeDiv');
+
+
+
+network.addEventListener("input", function () {
+    var select = document.getElementById('network');
+    var text = select.options[select.selectedIndex].text;
+    if (text == "MTN"){
+        dataType.removeAttribute("disabled")
+        gloDiv.style.display = "none"
+        etisalatDiv.style.display = "none"
+        airtelDiv.style.display = "none"
+    }
+    if (text == "GLO") {
+        dataType.setAttribute("disabled", "True")
+        gloDiv.style.display = "block"
+        etisalatDiv.style.display = "none"
+        airtelDiv.style.display = "none"
+    }
+    if (text == "9MOBILE") {
+        dataType.setAttribute("disabled", "True")
+        gloDiv.style.display = "none"
+        etisalatDiv.style.display = "block"
+        airtelDiv.style.display = "none"
+    }
+    if (text == "AIRTEL") {
+        dataType.setAttribute("disabled", "True")
+        gloDiv.style.display = "none"
+        etisalatDiv.style.display = "none"
+        airtelDiv.style.display = "block"
+    }
+})
+
+dataType.addEventListener("input", function() {
+    var select2 = document.getElementById('dataType');
+    var text2 = select2.options[select2.selectedIndex].text;
+    if (text2 == "CG1") {
+        mtnCG1.style.display = "block"
+        CG1options.removeAttribute("disabled")
+        mtnCG2.style.display = "none"
+        mtnSME.style.display = "none"
+        //alert("CG1 works")
+    }
+    if (text2 == "CG2") {
+        mtnCG1.style.display = "none"
+        mtnCG2.style.display = "block"
+        mtnSME.style.display = "none"
+        //alert("CG2 works")
+    }
+    if (text2 == "SME") {
+        mtnCG1.style.display = "none"
+        mtnCG2.style.display = "none"
+        mtnSME.style.display = "block"
+    }
+})
--- pages/register.js
+++ pages/register.js
@@ -1,118 +1,118 @@
-var fullname= document.getElementById('fullname');
-var fullname_error= document.getElementById('fullname_error');
+var fullname = document.getElementById('fullname');
+var fullname_error = document.getElementById('fullname_error');
 
-var username= document.getElementById('username');
-var username_error=document.getElementById('username_error');
+var username = document.getElementById('username');
+var username_error = document.getElementById('username_error');
 
-var email= document.getElementById('email');
-var email_error= document.getElementById('email_error');
+var email = document.getElementById('email');
+var email_error = document.getElementById('email_error');
 
-var tel= document.getElementById('tel');
-var tel_error= document.getElementById('tel_error');
+var tel = document.getElementById('tel');
+var tel_error = document.getElementById('tel_error');
 
-var password= document.getElementById('password');
-var password_error= document.getElementById('password_error');
+var password = document.getElementById('password');
+var password_error = document.getElementById('password_error');
 
-var confirmation= document.getElementById('confirm_password');
-var confirm_error= document.getElementById('confirm_error');
+var confirmation = document.getElementById('confirm_password');
+var confirm_error = document.getElementById('confirm_error');
 
-const togglePassword= document.getElementById('eye-icon1');
-const toggleConfirmPassword= document.getElementById('eye-icon2');
-var submit= document.getElementById('submit');
+const togglePassword = document.getElementById('eye-icon1');
+const toggleConfirmPassword = document.getElementById('eye-icon2');
+var submit = document.getElementById('submit');
 
-togglePassword.addEventListener("click", function () {
-            // toggle the type attribute
-            const type1 = password.getAttribute("type") === "password" ? "text" : "password";
-            password.setAttribute("type", type1)
-        });
+togglePassword.addEventListener("click", function() {
+    // toggle the type attribute
+    const type1 = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type1)
+});
 
-toggleConfirmPassword.addEventListener("click", function () {
-            // toggle the type attribute
-            const type2 = confirmation.getAttribute("type") === "password" ? "text" : "password";
-            confirmation.setAttribute("type", type2)
-        });
+toggleConfirmPassword.addEventListener("click", function() {
+    // toggle the type attribute
+    const type2 = confirmation.getAttribute("type") === "password" ? "text" : "password";
+    confirmation.setAttribute("type", type2)
+});
 
 
 submit.addEventListener("click", function(ev) {
     //Fullname Validation
     if (fullname.value == "") {
         ev.preventDefault()
-        fullname.style.borderColor="red"
-        fullname_error.innerHTML= "Cannot be blank"
+        fullname.style.borderColor = "red"
+        fullname_error.innerHTML = "Cannot be blank"
         if (fullname.addEventListener("focus", function() {
-            fullname_error.innerHTML= ""
-            fullname.style.borderColor="grey"
+            fullname_error.innerHTML = ""
+            fullname.style.borderColor = "grey"
         }))
-    alert("Useless line of code")
+            alert("Useless line of code")
     }
 
     //Username Validation
     if (username.value == "") {
         ev.preventDefault()
-        username.style.borderColor="red"
-        username_error.innerHTML= "Cannot be blank"
+        username.style.borderColor = "red"
+        username_error.innerHTML = "Cannot be blank"
         if (username.addEventListener("focus", function() {
-            username_error.innerHTML= ""
-            username.style.borderColor="grey"
+            username_error.innerHTML = ""
+            username.style.borderColor = "grey"
         }))
-    alert("Useless line of code")
+            alert("Useless line of code")
     }
 
     //Email Validation
     if (email.value == "") {
         ev.preventDefault()
-        email.style.borderColor="red"
-        email_error.innerHTML= "Cannot be blank"
+        email.style.borderColor = "red"
+        email_error.innerHTML = "Cannot be blank"
         if (email.addEventListener("focus", function() {
-            email_error.innerHTML= ""
-            email.style.borderColor="grey"
+            email_error.innerHTML = ""
+            email.style.borderColor = "grey"
         }))
-    alert("Useless line of code")
+            alert("Useless line of code")
     }
 
     //Phone number Validation 
     if (tel.value == "" || tel.value.length < 11 || tel.length > 11) {
         ev.preventDefault()
-        tel.style.borderColor="red"
-        tel_error.innerHTML= "Enter valid phone number"
+        tel.style.borderColor = "red"
+        tel_error.innerHTML = "Enter valid phone number"
         if (tel.addEventListener("focus", function() {
-            tel_error.innerHTML= ""
-            tel.style.borderColor="grey"
+            tel_error.innerHTML = ""
+            tel.style.borderColor = "grey"
         }))
-     alert("Useless line of code")
-     }
-    
+            alert("Useless line of code")
+    }
+
     //Password Validation
     if (password.value == "" || password.value.length < 8) {
         ev.preventDefault()
-        password.style.borderColor="red"
-        password_error.style.color= "red"
-        password_error.innerHTML= "Enter valid password"
+        password.style.borderColor = "red"
+        password_error.style.color = "red"
+        password_error.innerHTML = "Enter valid password"
         if (password.addEventListener("focus", function() {
-            password.style.borderColor="grey"
-            password_error.innerHTML= "Password should contain 8 characters or more"
-            password_error.style.color= "grey"
+            password.style.borderColor = "grey"
+            password_error.innerHTML = "Password should contain 8 characters or more"
+            password_error.style.color = "grey"
         }))
-     alert("Useless line of code")
-     }
+            alert("Useless line of code")
+    }
 
     //Password Confirmation
     if (confirmation.value !== password.value) {
         ev.preventDefault()
-        confirmation.style.borderColor="red"
-        confirm_error.style.color= "red"
-        confirm_error.innerHTML= "Passwords do not match"
+        confirmation.style.borderColor = "red"
+        confirm_error.style.color = "red"
+        confirm_error.innerHTML = "Passwords do not match"
         if (confirmation.addEventListener("focus", function() {
-            confirmation.style.borderColor="grey"
-            confirm_error.innerHTML= ""
-            confirm_error.style.color= "grey"
+            confirmation.style.borderColor = "grey"
+            confirm_error.innerHTML = ""
+            confirm_error.style.color = "grey"
         }))
-     alert("Useless line of code")
+            alert("Useless line of code")
     }
 
-    if(fullname.value !== "" && username.value !== "" && email.value !== "" && tel.value !== "" && tel.value.length == 11 && password.value.length >= 8 && confirmation.value == password.value){
-       ev.preventDefault()
-       window.open("dashboard.html", "_self")
+    if (fullname.value !== "" && username.value !== "" && email.value !== "" && tel.value !== "" && tel.value.length == 11 && password.value.length >= 8 && confirmation.value == password.value) {
+        ev.preventDefault()
+        window.open("dashboard.html", "_self")
 
 
         localStorage.setItem("fullname", fullname.value)
